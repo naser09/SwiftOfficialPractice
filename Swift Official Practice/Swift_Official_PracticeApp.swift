27f19b05ea1c81de44d4b0a0c90ce9c8b8ea2f8c
naser09 <75668297+naser09@users.noreply.github.com>
2022-06-09 18:25:27 +0600

    Initial Commit

---
+++ Swift Official Practice/Swift_Official_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Official_PracticeApp.swift
+//  Swift Official Practice
+//
+//  Created by abu naser on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Official_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
